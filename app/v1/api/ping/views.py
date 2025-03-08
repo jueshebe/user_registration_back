2574ebe0b3e54f6f2f2f18347dbc2e66765e5e26
JulianEstebanHerrera <julianesteban.herrera@mercadolibre.com.co>
2025-03-08 13:02:05 -0500

    final scaffolding finished

--- app/v1/api/ping/views.py
+++ app/v1/api/ping/views.py
@@ -1,4 +1,5 @@
 """Module with ping endpoint."""
+from logging import Logger
 from flask import Blueprint, Response
 
 
@@ -6,6 +7,7 @@ ping = Blueprint("ping", __name__)
 
 
 @ping.route("/ping")
-def main() -> Response:
+def main(logger: Logger) -> Response:
     """Ping endpoint, used to know if the app is up."""
+    logger.info("Ping endpoint called")
     return Response(response="pong", status=200, content_type="text/plain")
--- app/v1/module.py
+++ app/v1/module.py
@@ -1,14 +1,26 @@
 """Dependencies."""
 from flask_injector import singleton, Binder
-from logging import getLogger
+import logging
+import os
 
 
 def dependencies(binder: Binder) -> None:
     """Dependencies manager."""
-    logger = getLogger()
-    # binder.bind(
-    #     LoggerWithTags,
-    #     to=getLogger(),
-    #     scope=singleton,
-    # )
+    # Obtener el nivel de logs desde una variable de entorno
+    log_level = os.getenv("LOG_LEVEL", "INFO").upper()
+    level: int = getattr(logging, log_level, logging.INFO)
+
+    logger = logging.getLogger(__name__)
+    logger.setLevel(level)
+    handler = logging.StreamHandler()
+    handler.setLevel(level)
+    formatter = logging.Formatter("%(asctime)s - %(levelname)s - %(message)s")
+    handler.setFormatter(formatter)
+    logger.addHandler(handler)
+
+    binder.bind(
+        logging.Logger,
+        to=logger,
+        scope=singleton,
+    )
     logger.info("Dependencies manager finished")
